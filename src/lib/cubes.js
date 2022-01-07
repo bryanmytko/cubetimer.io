175fb56d8a3dbc7aa97fc81c4481c4df1fb5b11e
Bryan Mytko <bryanmytko@gmail.com>
2022-01-07 10:04:30 -0500

    Add Cube lib (3x3 only)

---
+++ src/lib/cubes.js
@@ -0,0 +1,8 @@
+export const cube3x3 = [
+  { name: 'R', turns: ['R', 'R\'', 'R2'], restricted: ['R', 'L'] },
+  { name: 'L', turns: ['L', 'L\'', 'L2'], restricted: ['L', 'R'] },
+  { name: 'U', turns: ['U', 'U\'', 'U2'], restricted: ['U', 'D'] },
+  { name: 'D', turns: ['D', 'D\'', 'D2'], restricted: ['D', 'U'] },
+  { name: 'F', turns: ['F', 'F\'', 'F2'], restricted: ['F', 'B'] },
+  { name: 'B', turns: ['B', 'B\'', 'B2'], restricted: ['B', 'F'] },
+];
