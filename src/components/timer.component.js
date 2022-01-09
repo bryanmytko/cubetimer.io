e62ff693013d4c8c8b34d790a351457f5074f1a6
Bryan Mytko <bryanmytko@gmail.com>
2022-01-09 18:13:09 -0500

    Add REMOVE_TIME action; Move calculations out of state machine

--- src/components/timer.component.js
+++ src/components/timer.component.js
@@ -3,15 +3,17 @@ import '../styles/timer.css';
 
 import Scramble from './scramble.component';
 import ScrambleService from '../services/scramble.service';
-import { formattedTime } from '../util/format';
 import TimerReducer from '../reducers/timer.reducer';
+import {
+  calcRunningAverage,
+  calcFastestTime,
+  calcSlowestTime,
+  formattedTime
+} from '../util/timer.util';
 
 const initialState = {
   running: false,
   ready: false,
-  fastestTime: 0,
-  slowestTime: 0,
-  runningAverage: 0,
   time: 0,
   solveTimes: [],
   scramble: ScrambleService.generate()
@@ -50,32 +52,41 @@ const Timer = () => {
   }, [state.running]);
 
   return <div className="container timer-container">
-    <Scramble scramble={state.scramble} />
-    <div className="solve-times card">
-      <ul>
-      {
-        state.solveTimes.map((t, index) => {
-          return <li key={index} className="time">{formattedTime(t)}</li>;
-        })
-      }
-     </ul>
-    </div>
-    <div className="timer-window card">
-      <span>{formattedTime(state.time, '0:00')}</span>
-    </div>
-    <button id="timer-btn"
-      className={`black-text btn timer-btn-start ${state.ready ? 'timer-btn-ready' : '' }`}
-      onClick={() => dispatch({ type: 'TOGGLE' })}>Press spacebar or click to begin!</button>
-    <div className="card row">
-      <div className="col s6 left-align">
-        <p>Cubes Solved: {state.solveTimes.length}</p>
-        <p>Average: {formattedTime(state.runningAverage)}</p>
+      <div className="row">
+        <div className="col s4">
+          <div className="solve-times card">
+            <h5>Solve Times:</h5>
+            <ul>
+            {
+              state.solveTimes.map((t, index) => {
+                return <li key={index} className="time">{formattedTime(t)}
+                <button onClick={() => dispatch({ type: 'REMOVE_TIME', index })}>[x]</button>
+                </li>;
+              })
+            }
+           </ul>
+          </div>
+        </div>
+        <div className="col s8">
+          <Scramble scramble={state.scramble} />
+          <div className="timer-window card">
+            <span>{formattedTime(state.time, '0:00')}</span>
+          </div>
+          <button id="timer-btn"
+            className={`black-text btn timer-btn-start ${state.ready ? 'timer-btn-ready' : '' }`}
+            onClick={() => dispatch({ type: 'TOGGLE' })}>Press spacebar or click to begin!</button>
+          <div className="card row">
+            <div className="col s6 left-align">
+              <p>Cubes Solved: {state.solveTimes.length}</p>
+              <p>Average: {formattedTime(calcRunningAverage(state.solveTimes))}</p>
+            </div>
+            <div className="col s6 right-align">
+              <p>Fastest: {formattedTime(calcFastestTime(state.solveTimes))}</p>
+              <p>Slowest: {formattedTime(calcSlowestTime(state.solveTimes))}</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col s6 right-align">
-        <p>Fastest: {formattedTime(state.fastestTime)}</p>
-        <p>Slowest: {formattedTime(state.slowestTime)}</p>
-      </div>
-    </div>
     </div>;
 }
 
--- src/reducers/timer.reducer.js
+++ src/reducers/timer.reducer.js
@@ -1,19 +1,6 @@
 import ScrambleService from '../services/scramble.service';
 
 const TimerReducer = (state, action) => {
-  const calcFastestTime = (times) => {
-    return Math.min(...times);
-  };
-
-  const calcSlowestTime = (times) => {
-    return Math.max(...times);
-  };
-
-  const calcRunningAverage = (times) => {
-    if(!times.length) return;
-    return times.reduce((prev, cur) => (prev + cur))/(times.length);
-  };
-
   switch(action.type){
     case 'TOGGLE':
       if(state.running) {
@@ -21,9 +8,6 @@ const TimerReducer = (state, action) => {
           ...state,
           running: false,
           ready: false,
-          fastestTime: calcFastestTime([...state.solveTimes, state.time]),
-          slowestTime: calcSlowestTime([...state.solveTimes, state.time]),
-          runningAverage: calcRunningAverage([...state.solveTimes, state.time]),
           solveTimes: [...state.solveTimes, state.time],
           scramble: ScrambleService.generate()
         };
@@ -31,6 +15,8 @@ const TimerReducer = (state, action) => {
       return { ...state, time: 0, ready: false, running: true };
     case 'READY':
       return state.running ? state : { ...state, ready: true };
+    case 'REMOVE_TIME':
+      return { ...state, solveTimes: state.solveTimes.filter((el, i, arr) => el !== arr[action.index])};
     case 'TICK':
       return { ...state, time: state.time + 10 };
     default:
