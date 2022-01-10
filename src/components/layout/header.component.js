91ce651e49a123ab21537d71873d323a848c2768
Bryan Mytko <bryanmytko@gmail.com>
2022-01-10 18:58:59 -0500

    Add top nav

--- src/components/layout/header.component.js
+++ src/components/layout/header.component.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Logo from '../layout/logo.component'
 
 const Header = () => {
-  return <header className="amber accent-4">
-    <div className="container">
-      <p className="black-text">Cubetimer.io</p>
-    </div>
+  return <header>
+    <nav className="amber accent-4">
+      <div class="container nav-wrapper">
+        <a href="#" class="brand-logo" id="logo">cubetimer.io</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="/">Timer</a></li>
+          <li><a href="/">Profile</a></li>
+          <li><a href="/">Tutorials</a></li>
+          <li><a href="/">Videos</a></li>
+          <li><a href="/">Puzzles</a></li>
+        </ul>
+      </div>
+    </nav>
   </header>
 };
 
