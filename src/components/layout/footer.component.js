35b95b5ef287c0c0887ab189106fe5e19caa7971
Bryan Mytko <bryanmytko@gmail.com>
2022-01-10 18:57:44 -0500

    Add date to footer

--- src/components/layout/footer.component.js
+++ src/components/layout/footer.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return <footer className="grey darken-4">
     <div className="container center-align white-text">
-      <p>&copy; 2022 &hearts; Bryan Mytko</p>
+      <p>&copy; 2014-{new Date().getFullYear()} &hearts; Bryan Mytko</p>
     </div>
   </footer>
 };
