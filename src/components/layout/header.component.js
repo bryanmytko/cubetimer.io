e8ccc9379b346bd8dd050367d2ce760a51736a49
Bryan Mytko <bryanmytko@gmail.com>
2022-01-11 01:38:13 -0500

    Reorganize header nav

--- src/components/layout/header.component.js
+++ src/components/layout/header.component.js
@@ -7,10 +7,12 @@ const Header = () => {
         <a href="/" className="brand-logo" id="logo">cubetimer.io</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="/">Timer</a></li>
+          <li><a href="/">Statistics</a></li>
           <li><a href="/">Profile</a></li>
           <li><a href="/">Tutorials</a></li>
-          <li><a href="/">Videos</a></li>
           <li><a href="/">Puzzles</a></li>
+          <li className="spacer">|</li>
+          <li><a href="/">Login</a></li>
         </ul>
       </div>
     </nav>
