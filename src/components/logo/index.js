6186f0a28c970bdea40a4bea3936b9eb39df9960
Bryan Mytko <bryanmytko@gmail.com>
2022-01-11 18:55:03 -0500

    Add Link to logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
-const Logo = () => (<div id="logo">cubetimer.io</div>);
+const Logo = () => (<div id="logo"><Link to="/">cubetimer.io</Link></div>);
 
 export default Logo;
