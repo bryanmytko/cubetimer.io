b3e4938cc5a905f8ea3d50596d4e46ff2ebd7b2d
Bryan Mytko <bryanmytko@gmail.com>
2022-01-11 18:16:30 -0500

    Add Statistics component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { default as Header } from './header';
 export { default as Logo } from './logo';
 export { default as Nav } from './nav';
 export { default as Scramble } from './scramble';
+export { default as Statistics } from './statistics';
 export { default as Timer } from './timer';
