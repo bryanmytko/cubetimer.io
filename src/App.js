f522e4c953c6fed7ad9b9edaf47634d9ebf4d12f
Bryan Mytko <bryanmytko@gmail.com>
2022-01-30 04:49:57 -0500

    Import React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import 'materialize-css/dist/css/materialize.min.css'
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import 'materialize-css/dist/css/materialize.min.css'
 
 import {
   Footer,
