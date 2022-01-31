75f072d68ccb6ea09e6fbbc73c72143885fde15c
Bryan Mytko <bryanmytko@gmail.com>
2022-01-30 21:22:06 -0500

    Add types; Update short circuit to return 0

--- src/services/calculate.service.ts
+++ src/services/calculate.service.ts
@@ -1,26 +1,26 @@
 class CalculateService {
-  runningAverage = (times) => {
-    if(!times || !times.length) return;
+  runningAverage = (times: Array<number>): number => {
+    if(!times || !times.length) return 0;
     return times.reduce((prev, cur) => (prev + cur))/(times.length);
   };
 
-  ao5 = (times) => {
-    if(!times || (times.length < 5)) return;
+  ao5 = (times: Array<number>): number => {
+    if(!times || (times.length < 5)) return 0;
     return times.slice(-5).reduce((prev, cur) => (prev + cur))/5;
   }
 
-  ao10 = (times) => {
-    if(!times || (times.length < 10)) return;
+  ao10 = (times: Array<number>):number => {
+    if(!times || (times.length < 10)) return 0;
     return times.slice(-10).reduce((prev, cur) => (prev + cur))/10;
   }
 
-  fastestTime = (times) => {
-    if(!times.length) return;
+  fastestTime = (times: Array<number>):number => {
+    if(!times.length) return 0;
     return Math.min(...times);
   };
 
-  slowestTime = (times) => {
-    if(!times.length) return;
+  slowestTime = (times: Array<number>): number => {
+    if(!times.length) return 0;
     return Math.max(...times);
   };
 }
